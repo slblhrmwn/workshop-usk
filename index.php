7ca23203e4c478b84213cf0962783c46776fa5fb
slblhrmwn <102720858+slblhrmwn@users.noreply.github.com>
2022-03-31 15:55:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>coffee shop</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<header>
+		<h1>Coffee shop Sisi Barat</h1>
+	</header>
+	<menu>
+		<a href="index.html">home</a>
+		<a href="contact.html">contact</a>
+		<a href="about.html">about</a>
+	</menu><br>
+	<article>
+		<h2>Daya Tarik Cafe Sisi Barat</h2>
+		<img src="coffee sisi barat.jpg">
+		<img src="foto3.jpg">
+		<p>Cafe ini merupakan salah satu dari sekian banyak objek wisata kuliner dan nongkrong di kota Bogor yang akhir-akhir ini viral di perbincangkan diberbagai sosial media. Sisi Barat Coffee ini dapat dibilang masih sangat baru karena Cafe ini baru grand opening pada tanggal 5 Desember 2021 lalu.</p>
+
+		<p>Sisi Barat Coffee ini hadir dengan menyediakan 2 tempat yaitu cafe indoor dan juga outdoor. Cafe ini membawa konsep industrial cafe dengan interior yang simple minimalis serta memberikan kenyamanan bagi pengunjung agar betah menghabiskan waktu di lokasi tersebut.</p> 
+
+		<p>Daya tarik yang ada di Cafe Sisi Barat Coffee ini yaitu tempatnya yang menampilkan pemandangan pesona alam yang indah di sekitar cafe. Hal ini di karenakan lokasinya yang berada tepat disamping Sungai Cisandang Barang dan terdapat banyak pohon yang membuat sejuk di bagian outdoor Sisi Barat Coffee. Wisatawan yang berkunjung akan dapat melihat pemandangan alam berupa aliran sungai dengan suara gemericik air dan membuat hati tenang serta betah untuk berlama-lama di Cafe ini.</p>
+
+		<p>Dari kejauhan anda juga dapat melihat pemandangan Gunung Salak megah dan indah. Selain itu, pepohonan hijau yang rindang dan mengelilingi lokasi cafe Sisi Barat Coffee ini membuat suasana begitu sejuk dan asri. Desain tempatnya yang simpel minimalis serta bagus membuat Cafe ini sangat menarik untuk di foto, terdapat berbagai spot foto menarik yang bagus untuk anda abadikan dan posting di berbagai media sosial. Pastinya siapapun pengunjung Sisi Barat Coffee akan tergoda untuk berfoto di lokasi ini.</p>
+
+		<p>Untuk menu makanan dan minuman di Sisi Barat Coffee ini juga sudah komplit dan memiliki harga yang cukup terjangkau. Hal ini juga menjadi alasan dan daya tarik bagi pengungjung Sisi Barat Coffee.</p> 
+
+	<h2>Fasilitas Umum Yang Ada di Sisi Barat Coffee</h2>
+		<p>Demi menunjang kenyamanan para pelanggan yang berkunjung. Sisi Barat Coffee telah menyediakan berbagai macam fasilitas umum yang dapat anda gunakan selama berkunjung ke Cafe ini. berikut beberapa fasilitas umum yang akan anda dapatkan di Sisi Barat Coffee.
+		<ul>
+		<li>Area Parkir</li>
+		<li>Toilet  Umum</li>
+		<li>Mushola</li>
+		<li>Akses Free Wifi</li>
+		<li>Gazebo</li>
+		<li>Spot Foto</li>
+		<li>Berbagai lokasi tempat duduk</li>
+		<li>Live Music</li>
+		<li>Dan masih banyak lagi lainnya</li>
+		</ul>
+</p> 
+
+<p>
+	<h2>Lokasi Sisi Barat Coffee</h2>
+	<p>
+	Bagi kalian yang ingin berkunjung ke Sisi Barat Coffee ini, anda dapat pergi ke lokasinya yang berada di Jalan. Batu Hulung, RT.02/RW.02, Kelurahan Balungbangjaya, Kecamatan Bogor Barat,  Kota Bogor, Jawa Barat. Untuk akses jalannya menuju ke lokasi ini sangatlah mudah, karena lokasinya yang tidak jauh dari pusat kota Bogor. Hanya berjarak sekitar kurang lebih 10 km dengan waktu tempuh sekitar 25 menit menggunakan kendaraan bermotor.</p> 
+
+	<p>Bagi kalian yang bingung untuk mencari lokasinya, maka anda dapat melihat peta lokasinya pada link berikut, guna menentukan rute perjalanan dari lokasi anda menuju ke lokasi Sisi Barat Coffee Bogor.</p>	
+
+		</p>
+		<p>
+			<h2>Galeri Foto Cafe Sisi Barat</h2>
+			<img src="foto1.jpg">
+			<img src="foto4.jpg">
+			<img src="foto2.jpg">
+			<img src="foto5.jpg">
+		</p>
+	</article>
+	<footer>
+		<p>@Copyright 2021-SH</p>
+	</footer>
+</body>
+</html>
